508fb5130202d7642a5f12c421a07ebed8af07d4
Timchenko Bogdan <tbd2504@gmail.com>
2023-05-09 11:35:29 +0300

    Add model

--- src/core/settings.py
+++ src/core/settings.py
@@ -154,9 +154,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-
+AUTH_USER_MODEL = 'accounts.user'
 
 if DEBUG:
     INTERNAL_IPS = [
