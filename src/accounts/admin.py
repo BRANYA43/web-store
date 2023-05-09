e697be5f6485ef365ec3c741d3abcc74ee219ef3
Timchenko Bogdan <tbd2504@gmail.com>
2023-05-09 11:48:19 +0300

    Add admin

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+@admin.register(get_user_model())
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email')
+    exclude = ('password',)
+    fieldsets = (
+        ('Personal info', {'fields': ('username', 'email', 'first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_superuser', 'is_staff', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    readonly_fields = ('last_login', 'date_joined', 'is_active')
+
