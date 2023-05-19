7cfcbc398d369544acd0817f54e43fbba4c90fb4
Timchenko Bogdan <tbd2504@gmail.com>
2023-05-19 11:16:31 +0300

    Add order and order item

--- src/orders/admin.py
+++ src/orders/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from orders.models import OrderItem, Order
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+    can_delete = False
+
+    def has_add_permission(self, request, obj):
+        return False
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderItemInline,)
