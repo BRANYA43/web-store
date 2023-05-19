66f48f0de2c85772b5daf4a8ddbee1a04efd3807
Timchenko Bogdan <tbd2504@gmail.com>
2023-05-19 10:10:06 +0300

    Add save quantity item

--- src/carts/forms.py
+++ src/carts/forms.py
@@ -1,1 +1,17 @@
 from django import forms
+from django.forms import modelformset_factory
+
+from carts.models import CartItem
+
+
+class CartItemForm(forms.ModelForm):
+    class Meta:
+        model = CartItem
+        fields = ('cart', 'good', 'quantity')
+        widgets = {
+            'cart': forms.HiddenInput,
+            'good': forms.HiddenInput,
+        }
+
+
+CartItemFormSet = modelformset_factory(CartItem, CartItemForm, extra=0)
--- src/carts/views.py
+++ src/carts/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.views import generic
 
 from carts.forms import CartItemFormSet
@@ -14,4 +15,18 @@ class CartView(LoginRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         return self.model.objects.filter(cart=self.request.user.cart)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['formset'] = self.formset(queryset=self.get_queryset())
+        return context
+
+    def post(self, request, *args, **kwargs):
+        formset = self.formset(request.POST)
+        print(request.POST)
+        print('pre save')
+        if formset.is_valid():
+            print('save')
+            formset.save()
+        return redirect(reverse_lazy('carts:cart'))
 
