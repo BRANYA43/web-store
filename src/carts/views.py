4a78ffb26002744041e06dc5a1dd4eae8bba2b15
Timchenko Bogdan <tbd2504@gmail.com>
2023-05-19 09:31:00 +0300

    Add cart item list

--- src/carts/views.py
+++ src/carts/views.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from carts.forms import CartItemFormSet
+from carts.models import CartItem
+
+
+class CartView(LoginRequiredMixin, generic.ListView):
+    model = CartItem
+    template_name = 'carts/item_list.html'
+    context_object_name = 'items'
+    formset = CartItemFormSet
+
+    def get_queryset(self):
+        return self.model.objects.filter(cart=self.request.user.cart)
+
