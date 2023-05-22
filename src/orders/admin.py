d0d2b68c4d4090bbf4dd6805a3773dfa12fc4570
Timchenko Bogdan <tbd2504@gmail.com>
2023-05-22 12:47:58 +0300

    Refactor admin

--- src/orders/admin.py
+++ src/orders/admin.py
@@ -5,18 +5,19 @@ from orders.models import OrderItem, Order
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
+    readonly_fields = ('title', 'slug', 'quantity', 'price', 'total')
     extra = 0
     can_delete = False
 
     def has_add_permission(self, request, obj):
         return False
 
-
-@admin.register(OrderItem)
-class OrderItemAdmin(admin.ModelAdmin):
-    ...
+    def total(self, instance):
+        return instance.get_total()
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
+    list_display = ('uuid', 'user', 'is_paid', 'created')
+    readonly_fields = ('uuid', 'user', 'is_paid', 'created')
     inlines = (OrderItemInline,)
