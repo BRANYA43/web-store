d69dab0d09c09d3a0f759ea4f96ed98a82824486
Timchenko Bogdan <tbd2504@gmail.com>
2023-05-22 11:45:50 +0300

    Add order history

--- src/orders/urls.py
+++ src/orders/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from orders import views
+
 app_name = 'orders'
 
 urlpatterns = [
-
+    path('history/', views.OrderListView.as_view(), name='history'),
 ]
--- src/orders/views.py
+++ src/orders/views.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from orders.models import Order
+
+
+class OrderListView(LoginRequiredMixin, generic.ListView):
+    model = Order
+    template_name = 'orders/list.html'
+    context_object_name = 'orders'
+
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
+
